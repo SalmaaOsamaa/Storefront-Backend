060368bf249a2b55a8f2872555ebd8d2d3ddffc1
SalmaaOsamaa <salma.usama1996@gmail.com>
2022-06-20 03:52:53 +0200

    edits

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
+import morgan from 'morgan';
 import bodyParser from "body-parser";
 import cors from "cors";
 // import { productRouter } from './routes/api/products'
 import routes from "./routes";
+import client from './database';
 export const app: express.Application = express();
 const address  = "0.0.0.0:3000";
-
+app.use(morgan('common'));
 app.use(bodyParser.json());
 
 const corsOptions = {
@@ -19,7 +21,20 @@ app.use("/api", routes);
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
+client.connect().then((client) => {
+  return client
+    .query("SELECT NOW()")
+    .then((res) => {
+      client.release();
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      client.release();
+      console.log(err.stack);
+    });
+});
 
+app.use("/api", routes);
 app.listen(3000,  ()=> {
   console.log(`starting app on: ${address}`);
 });
